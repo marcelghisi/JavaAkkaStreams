73c9ccf11d1d712e4c376f572e48f5f1c1258a56
Marcel Ghisi <marcel.ghisi@gmail.com>
2025-11-25 20:46:40 +0100

    Infinite iterators

--- src/main/java/SimpleStream.java
+++ src/main/java/SimpleStream.java
@@ -7,8 +7,10 @@ import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
+import java.util.stream.Stream;
 
 public class SimpleStream {
 
@@ -18,9 +20,11 @@ public class SimpleStream {
         List<String> names = List.of("James","Jill","Simon","Saly","Denise","David");
 
         Source<String, NotUsed> source = Source.from(names);
-
         Source<Double, NotUsed> piSource = Source.repeat(3.141592654);
         Source<String, NotUsed> repeatNameSource = Source.cycle(names::iterator);
+
+        Iterator<Integer> infiniteRange = Stream.iterate(0, i ->  i + 1).iterator();
+        Source<Integer, NotUsed> infiniteRangeSource = Source.fromIterator(() -> infiniteRange);
 
         Flow<Integer,String, NotUsed> flow = Flow.of(Integer.class).map(value -> "The next value is " + value);
         Flow<String,String, NotUsed> stringFlow = Flow.of(String.class).map(value -> "The next value is " + value);
@@ -29,7 +33,7 @@ public class SimpleStream {
             System.out.println(value);
         }) ;
 
-        RunnableGraph<NotUsed> graph = repeatNameSource.via(stringFlow).to(sink);
+        RunnableGraph<NotUsed> graph = infiniteRangeSource.via(flow).to(sink);
 
         ActorSystem actorSystem = ActorSystem.create(Behaviors.empty(), "actorSystem");
 
