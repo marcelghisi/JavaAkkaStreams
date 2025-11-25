99838d9e418f768abe74f381550932df3cb5481b
Marcel Ghisi <marcel.ghisi@gmail.com>
2025-11-25 20:21:26 +0100

    List of names source

--- src/main/java/SimpleStream.java
+++ src/main/java/SimpleStream.java
@@ -7,6 +7,7 @@ import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 public class SimpleStream {
@@ -14,15 +15,17 @@ public class SimpleStream {
     public static void main(String[] args) {
         //Source<Integer, NotUsed> source = Source.range(1,10);
 
-        Source<Integer, NotUsed> source = Source.single(17);
+        List<String> names = List.of("James","Jill","Simon","Saly","Denise","David");
+
+        Source<String, NotUsed> source = Source.from(names);
 
         Flow<Integer,String, NotUsed> flow = Flow.of(Integer.class).map(value -> "The next value is " + value);
-
+        Flow<String,String, NotUsed> stringFlow = Flow.of(String.class).map(value -> "The next value is " + value);
         Sink<String, CompletionStage<Done>> sink = Sink.foreach(value -> {
             System.out.println(value);
         }) ;
 
-        RunnableGraph<NotUsed> graph = source.via(flow).to(sink);
+        RunnableGraph<NotUsed> graph = source.via(stringFlow).to(sink);
 
         ActorSystem actorSystem = ActorSystem.create(Behaviors.empty(), "actorSystem");
 
