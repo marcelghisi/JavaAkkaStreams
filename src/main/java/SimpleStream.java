29b0fa87be14afc89d246eda2a7a0c5724255666
Marcel Ghisi <marcel.ghisi@gmail.com>
2025-11-25 20:52:06 +0100

    Infinite throttle and duration

--- src/main/java/SimpleStream.java
+++ src/main/java/SimpleStream.java
@@ -7,6 +7,7 @@ import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
 
+import java.time.Duration;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
@@ -24,7 +25,7 @@ public class SimpleStream {
         Source<String, NotUsed> repeatNameSource = Source.cycle(names::iterator);
 
         Iterator<Integer> infiniteRange = Stream.iterate(0, i ->  i + 1).iterator();
-        Source<Integer, NotUsed> infiniteRangeSource = Source.fromIterator(() -> infiniteRange);
+        Source<Integer, NotUsed> infiniteRangeSource = Source.fromIterator(() -> infiniteRange).throttle(1, Duration.ofSeconds(3));
 
         Flow<Integer,String, NotUsed> flow = Flow.of(Integer.class).map(value -> "The next value is " + value);
         Flow<String,String, NotUsed> stringFlow = Flow.of(String.class).map(value -> "The next value is " + value);
